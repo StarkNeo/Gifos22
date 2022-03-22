f71e9be0598425d8b76b7d945aef6e51e93864dd
Jesus Hdz <jesushdzusa@gmail.com>
2022-03-22 02:57:33 -0500

    modo nocturno aplicado a todas las paginas con localstorage

--- scripts/nocturno.js
+++ scripts/nocturno.js
@@ -1,36 +1,54 @@
 var vista;
-var estilo=document.querySelector('link','status');
+var estilo = document.querySelector('link', 'status');
 
 if (!localStorage.modo) {
-    vista='modoDiu';
-    localStorage.setItem('modo',vista);
+    vista = 'modoDiu';
+    localStorage.setItem('modo', vista);
 }
 else {
- vista=localStorage.getItem('modo');
+    vista = localStorage.getItem('modo');
 }
 
 function decision() {
-    
-    if (vista==='modoDiu') {
-        estilo.href='/styles/styles.css';
+    let logo = document.getElementById('logo');
+    let modo = document.getElementsByTagName('li')[0];
+    let iconoClose = document.getElementById('iconoClose');
+    let iconoLupa = document.getElementById('iconoLupa');
+    //let btnsliderLeft = document.getElementById('sleft');
+    //let btnsliderRight = document.getElementById('sright');
+    let btnmas = document.getElementById('mas');
+
+
+
+    if (vista === 'modoDiu') {
+        estilo.href = '/styles/styles.css';
+        logo.src = "/images/logo-desktop.svg";
+        modo.innerHTML = 'MODO NOCTURNO';
     } else {
-        estilo.href='/styles/nocturno.css';
+        estilo.href = '/styles/nocturno.css';
+        modo.innerHTML = 'MODO DIURNO';
+        logo.src = "/images/Logo-modo-noc.svg";
+        //iconoClose.src = '/images/icon-search-modo-noct.svg';
+        //iconoLupa.src = '/images/icon-search-modo-noct.svg';
+        //btnsliderLeft.src = '/images/button-slider-left-md-noct.svg';
+        //btnsliderRight.src = '/images/button-slider-right-md-noct.svg';
+        btnmas.src = '/images/CTA-ver+-modo-noc.svg';
     }
 }
 
 decision();
-document.addEventListener('click',e=>{
-    if (e.target.className==='modoDiu') {
-        e.target.className='modoNoc';
-        vista='modoNoc';
-        
+document.addEventListener('click', e => {
+    if (e.target.className === 'modoDiu') {
+        e.target.className = 'modoNoc';
+        vista = 'modoNoc';
+
     }
-    else if(e.target.className==='modoNoc'){
-        e.target.className='modoDiu';
-        vista='modoDiu';
+    else if (e.target.className === 'modoNoc') {
+        e.target.className = 'modoDiu';
+        vista = 'modoDiu';
 
         //localStorage.setItem('modo',vista);      
     }
-    localStorage.setItem('modo',vista);
+    localStorage.setItem('modo', vista);
     decision();
 })
